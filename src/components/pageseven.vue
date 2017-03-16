625b48f647a9ceca1ab2c9bae4223e19ba17ba42
lizhi <935112464@qq.com>
2017-03-16 15:57:36 +0800

    更改

--- src/components/pageseven.vue
+++ src/components/pageseven.vue
@@ -1,6 +1,6 @@
 <template>
 <div class="container">
-    <div class="content" v-show="content">
+    <div id="opa" :class="{animated:true,content:true,fadeOutLeft:fadeOutLeft}">
       <p class="say">对名朋想说的话</p>
       <ul class="list" @touchmove.stop>
         <li v-for="item in items">
@@ -12,10 +12,18 @@
         </li>
       </ul>
     </div>
-    <div class="content" v-show="!content">
+    <div id="zindex" :class="{animated:true,content:true,fadeInRight:fadeOutLeft}">
       <img src="./../assets/Group.png" class="group">
+      <div class="wish_content">
+          <textarea maxlength="100" v-model="val" placeholder="输入你想对名朋说的话"></textarea>
+          <div class="changeuser">
+            <div></div>
+            <div class="check_name">艾薇儿 NO.5214</div>
+            <div :class="{active:changeuser}" @touchstart="f_touch_change" @touchend="f_touch_change" @click="f_change"><img src="./../assets/change.png">切换身份</div>
+          </div>
+      </div>
     </div>
-    <button :class="{wish:true,active:active}" @touchstart="f_touch($event)" @touchend="f_touch($event)">祝福名朋</button>
+    <button :class="{wish:true,active:active}" @touchstart="f_touch($event)" @touchend="f_touch($event)" @click="push_wish">{{str}}</button>
     <p :class="{share:true,active:act}" @touchstart="f_touch($event)" @touchend="f_touch($event)">分享给小伙伴</p>
 </div>
 </template>
@@ -27,7 +35,11 @@
         time:false,
         active:false,
         act:false,
-        content:false,
+        content:true,
+        changeuser:false,
+        val:'',
+        str:'祝福名朋',
+        fadeOutLeft:false,
         items:[
           {
             headimg:'',
@@ -82,10 +94,23 @@
         },1000)
       },
       f_touch:function(event){
-        if(event.target.innerHTML=='祝福名朋'){
+        if(event.target.innerHTML=='祝福名朋' || event.target.innerHTML=='写下祝福'){
           this.active=!this.active;
         }else{
           this.act=!this.act;
+        }
+      },
+      f_touch_change:function(){
+        this.changeuser=!this.changeuser;
+      },
+      f_change:function(){
+
+      },
+      push_wish:function(){
+        if(this.str=='祝福名朋'){
+          this.fadeOutLeft=true;
+          this.str="写下祝福";
+        }else{
         }
       },
 
@@ -133,7 +158,16 @@
  }
  .content{
   height:9.1rem;
-  position: relative;
+  width:100%;
+  position: absolute;
+  top:0;
+  left:0;
+ }
+ #opa{
+  opacity: 1;
+ }
+ #z-index{
+  z-index: 999;
  }
  .con>p:nth-child(1){
   color:#F8E81C;
@@ -150,6 +184,7 @@
  }
  .wish{
   display: block;
+  position: absolute;
   width:4rem;
   height:.88rem;
   color:#fff;
@@ -159,14 +194,15 @@
     background: linear-gradient(left,#01C3FF,#777DFA);
   background: -webkit-linear-gradient(left,#01C3FF,#777DFA);
   border:none;
-  margin:0 auto;
-    margin-top:.25rem;
+  left:1.75rem;
+  top:9.35rem;
     outline: none;
  }
  .share{
   font-size:.28rem;
   color:#6BCEF7;
-  margin-top:.2rem;
+  position: absolute;
+  top:10.4rem;
   text-align:center;
  }
  .wish.active{
@@ -177,11 +213,72 @@
   transform:scale(.95,.95);
   -webkit-transform:scale(.95,.95);
   }
+  .changeuser>div:nth-child(3).active{
+  transform:scale(.95,.95);
+  -webkit-transform:scale(.95,.95);
+  }
   .group{
     position: absolute;
     display: block;
     width:6.3rem;
     bottom:0;
     left:0;
+  }
+  .wish_content{
+    position: absolute;
+    bottom:0;
+    width:6.3rem;
+    height:7.88rem;
+    padding:.5rem .4rem;
+    background: transparent;
+  }
+  textarea{
+    background: transparent;
+    color:#fff;
+    font-size:.32rem;
+    line-height:.48rem;
+    outline: none;
+    border: none;
+    resize: none;
+    display: block;
+    width:100%;
+    height:6.2rem;
+  }
+  .changeuser{
+    width:100%;
+    height:.48rem;
+  }
+  .changeuser>div:nth-child(1){
+    float: left;
+    width:.48rem;
+    height:.48rem;
+    background: white;
+    border-radius: 50%;
+  }
+  .changeuser>div:nth-child(2){
+    float: left;
+    color:#F8E81C;
+    font-size:.24rem;
+    line-height:.48rem;
+    margin-left:.2rem;
+    max-width: 3rem;
+    white-space:  nowrap;
+    overflow:hidden;
+    text-overflow:ellipsis;
+  }
+  .changeuser>div:nth-child(3){
+    float: right;
+    font-size:.24rem;
+    line-height:.48rem;
+    color:#00E2FD;
+    overflow:hidden;
+    height:100%;
+  }
+  .changeuser>div:nth-child(3) img{
+    display: inline-block;
+    width:.4rem;
+    position: relative;
+    top:.05rem;
+    margin-right:.1rem;
   }
 </style>
